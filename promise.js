df10a142a8b6f472a98ea170e80ca8dc6f34b6ce
singhaman779 <sftersinghaman@gmail.com>
2023-12-20 14:27:58 +0530

    updated

--- promise.js
+++ promise.js
@@ -1,5 +1,3 @@
-// promise.js
-
 const Posts = [
     { title: 'post1', body: 'this is post1' },
     { title: 'post2', body: 'this is post2' }
@@ -88,4 +86,3 @@ const Posts = [
   
   // Call main function 
   main();
-  
